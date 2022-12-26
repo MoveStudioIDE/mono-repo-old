6aa4a7c7e531a7e0e31bb5fc61dac7c9ae122a11
dantheman8300 <daniel.7039@hotmail.com>
2022-12-26 10:36:11 -0800

    got basic monaco editor

--- studio-frontend/src/components/BuildCanvas.tsx
+++ studio-frontend/src/components/BuildCanvas.tsx
@@ -1,5 +1,7 @@
 import { useEffect } from "react";
 import "./BuildCanvas.css";
+import CodeEditorWindow from "./Editor";
+import Editor from "@monaco-editor/react";
 
 
 function BuildCanvas(
@@ -11,27 +13,50 @@ function BuildCanvas(
 
   //---Effects---//
 
-  useEffect(() => {
-    console.log('Canvas useEffect', props.code)
-    const editor = document.getElementById('program') as HTMLTextAreaElement;
-    editor.value = props.code;
-  }, [props.code])
+  // useEffect(() => {
+  //   console.log('Canvas useEffect', props.code)
+  //   const editor = document.getElementById('program') as HTMLTextAreaElement;
+  //   editor.value = props.code;
+
+  //   const newEditor = document.getElementById('editor');
+
+  //   console.log("editor", editor)
+  //   console.log("new editor", newEditor)
+
+  //   // newEditor.defaultValue = props.code;
+  // }, [props.code])
 
   //---Handlers---//
 
-  const handleTextChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
-    props.setCode(event.target.value);
-  }
+  // const handleTextChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+  //   props.setCode(event.target.value);
+  // }
+
+  const handleEditorChange = (value: any) => {
+    props.setCode(value);
+  };
 
   //---Render---//
 
   return (
-    <textarea
-      id="program"
-      className="editor"
-      onChange={handleTextChange}
-    >
-    </textarea>
+    <div>
+      {/* <textarea
+        id="program"
+        className="editor"
+        onChange={handleTextChange}
+      >
+      </textarea> */}
+      {/* <p>test</p> */}
+      <Editor
+        height="85vh"
+        width={`100%`}
+        language={"javascript"}
+        value={props.code}
+        onChange={handleEditorChange}
+        // theme="vs-dark"
+      />
+    </div>
+    // <CodeEditorWindow/>
   )
 }
 
