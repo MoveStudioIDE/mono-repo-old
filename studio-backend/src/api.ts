ca6d429bf17c9873460393eee5d09efb1f166647
dantheman8300 <daniel.7039@hotmail.com>
2023-01-03 11:56:49 -0800

    put port number back

--- studio-backend/src/api.ts
+++ studio-backend/src/api.ts
@@ -4,7 +4,7 @@ import { compile, publish } from './compile';
 import { getObjectDetails, getPackageDetails } from './object-details';
 
 const app = express();
-const port = 5000;
+const port = 5001;
 
 app.use(cors());
 app.use(express.json());
