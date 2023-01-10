541de77a2e275d083d97cd8a897678e6ad8df36e
dantheman8300 <daniel.7039@hotmail.com>
2023-01-10 10:18:20 -0800

    improve background of canvases

--- studio-frontend/src/components/DeployHeader.tsx
+++ studio-frontend/src/components/DeployHeader.tsx
@@ -64,16 +64,17 @@ function DeployHeader(
       
       <div className="flex-none">
         <div className="dropdown dropdown-end ">
-          <label tabIndex={0} className="btn btn-ghost rounded-btn mr-2">Theme</label>
+          <label tabIndex={0} className="btn btn-ghost rounded-btn mr-2 normal-case text-lg">Theme</label>
           <ul tabIndex={0} className="menu dropdown-content menu-compact p-2 shadow bg-base-100 rounded-box w-52 mt-4 h-40" style={{display: "inline", overflow: "auto"}}>
             {
               daisyThemes.map((theme) => {
-                return <li><a className={`btn m-1 btn-secondary ${props.theme === 'winter' ? 'text-slate-400' : ''}`} onClick={() => {props.setTheme(theme)}}>{theme}</a></li> 
+                // return <li><a className={`btn m-1 btn-secondary ${props.theme === 'winter' ? 'text-slate-400' : ''}`} onClick={() => {props.setTheme(theme)}}>{theme}</a></li> 
+                return <li><a className={`btn m-1 btn-accent`} style={{color: "hsl(var(--ac))"}} onClick={() => {props.setTheme(theme)}}>{theme}</a></li> 
               })
             }
           </ul>
         </div>
-        <ConnectButton className="mr-2 btn btn-md" style={{backgroundColor: "hsl(var(--a))", color: "hsl(var(--b1))"}}/>
+        <ConnectButton className="mr-2 btn btn-md" style={{backgroundColor: "hsl(var(--a))", color: "hsl(var(--ac))"}}/>
         {/* <button className="btn btn-square btn-ghost">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-5 h-5 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path></svg>
         </button> */}
