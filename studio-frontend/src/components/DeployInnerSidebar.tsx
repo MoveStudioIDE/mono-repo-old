aff3537f819dd993c8e678806f455b0c821510d2
dantheman8300 <daniel.7039@hotmail.com>
2023-04-26 19:45:00 -0700

    commented out breaking stuff

--- studio-frontend/src/components/DeployInnerSidebar.tsx
+++ studio-frontend/src/components/DeployInnerSidebar.tsx
@@ -190,7 +190,7 @@ function DeployInnerSidebar(
             <div>
               <h2 className='font-semibold'>Gas balance:</h2>
               <p className="text-center text-neutral-content font-mono text-opacity-90">
-                {loading ? "Loading balance..." : `${decimalify(balance, 9)} Sui`} 
+                {/* {loading ? "Loading balance..." : `${decimalify(balance, 9)} Sui`} 
                 {!loading &&
                   <label 
                     tabIndex={0} 
@@ -202,7 +202,7 @@ function DeployInnerSidebar(
                   >
                     <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round"><path d="M16 4h2a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h2"></path><rect x="8" y="2" width="8" height="4" rx="1" ry="1"></rect></svg>
                   </label>
-                }
+                } */}
               </p>
             </div>
             <div>
